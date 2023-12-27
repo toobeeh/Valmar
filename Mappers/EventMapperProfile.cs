6ac980299039be2d7268d89a099c18097e947d6d
tobeh <tobiasch@gmx.at>
2023-12-28 00:26:22 +0100

    fix missing event entity mappings

--- Mappers/EventMapperProfile.cs
+++ Mappers/EventMapperProfile.cs
@@ -7,7 +7,23 @@ public class EventMapperProfile : Profile
 {
     public EventMapperProfile()
     {
-        CreateMap<EventEntity, EventReply>();
-        CreateMap<EventDropEntity, EventDropReply>();
+        CreateMap<EventEntity, EventReply>()
+            .ForMember(
+                evt => evt.Id,
+                opt => opt.MapFrom(src => src.EventId))
+            .ForMember(
+                evt => evt.Start,
+                opt => opt.MapFrom(src => src.ValidFrom))
+            .ForMember(
+                evt => evt.Name,
+                opt => opt.MapFrom(src => src.EventName))
+            .ForMember(
+                evt => evt.Length,
+                opt => opt.MapFrom(src => src.DayLength));
+        
+        CreateMap<EventDropEntity, EventDropReply>()
+            .ForMember(
+                drop => drop.Id, 
+                opt=> opt.MapFrom(src => src.EventDropId));
     }
 }
