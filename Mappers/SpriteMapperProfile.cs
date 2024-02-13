88f9aa4ace41d5d822290baac38e30a5ac82ac83
tobeh <tobiasch@gmx.at>
2024-02-13 12:30:38 +0100

    fix mapping of sprite details

--- Mappers/SpriteMapperProfile.cs
+++ Mappers/SpriteMapperProfile.cs
@@ -8,6 +8,10 @@ public class SpriteMapperProfile : Profile
     public SpriteMapperProfile()
     {
         CreateMap<SpriteEntity, SpriteReply>()
+            .ForMember(s => s.IsRainbow, 
+                opt => opt.MapFrom(src => src.Rainbow == 1))
+            .ForMember(s => s.IsSpecial, 
+                opt => opt.MapFrom(src => src.Special))
             .ForMember(s => s.EventDropId, 
                 opt => opt.MapFrom(src => MapEventDropId(src)))
             .ForMember(s => s.Artist, 
