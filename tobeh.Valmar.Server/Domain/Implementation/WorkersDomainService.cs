a98c6a3c46f32f66d58333a725e21f966308455f
tobeh <tobiasch@gmx.at>
2024-05-08 00:40:48 +0200

    change presets for new guild options

--- tobeh.Valmar.Server/Domain/Implementation/WorkersDomainService.cs
+++ tobeh.Valmar.Server/Domain/Implementation/WorkersDomainService.cs
@@ -6,7 +6,7 @@ using tobeh.Valmar.Server.Domain.Exceptions;
 namespace tobeh.Valmar.Server.Domain.Implementation;
 
 public class WorkersDomainService(
-    ILogger<WorkersDomainService> logger, 
+    ILogger<WorkersDomainService> logger,
     IMembersDomainService membersDomainService,
     PalantirContext db) : IWorkersDomainService
 {
@@ -17,7 +17,7 @@ public class WorkersDomainService(
         var instances = await db.LobbyBotInstances.ToListAsync();
 
         var unclaimedInstances = instances.Where(InstanceUnclaimedOrExpired);
-        
+
         var instance = unclaimedInstances.FirstOrDefault();
         if (instance is null)
         {
@@ -26,7 +26,7 @@ public class WorkersDomainService(
 
         return instance;
     }
-    
+
     public async Task<LobbyBotInstanceEntity> GetUnclaimedInstanceForServer()
     {
         logger.LogTrace("GetUnclaimedInstanceForServer()");
@@ -36,7 +36,8 @@ public class WorkersDomainService(
         var memberPermissions =
             (await membersDomainService.GetMembersByLogin(memberClaims.Select(member => member.Login).ToList()))
             .ToDictionary(member => member.Login, member => member.MappedFlags.Contains(MemberFlagDdo.Patron));
-        var validMemberClaims = memberClaims.Where(claim => memberPermissions[claim.Login]).Select(claim => claim.InstanceId).ToList();
+        var validMemberClaims = memberClaims.Where(claim => memberPermissions[claim.Login])
+            .Select(claim => claim.InstanceId).ToList();
 
         var freeInstances = instances.Where(instance => !validMemberClaims.Contains(instance.Id));
         var instance = freeInstances.FirstOrDefault();
@@ -47,7 +48,7 @@ public class WorkersDomainService(
 
         return instance;
     }
-    
+
     public async Task<LobbyBotInstanceEntity> GetInstanceById(int instanceId)
     {
         logger.LogTrace("GetInstanceById(instanceId={instanceId})", instanceId);
@@ -60,8 +61,9 @@ public class WorkersDomainService(
 
         return instance;
     }
-    
-    public async Task<LobbyBotInstanceEntity> ClaimInstanceForWorker(string workerUlid, int instanceId, string claimUlid, string? lastClaimUlid = null)
+
+    public async Task<LobbyBotInstanceEntity> ClaimInstanceForWorker(string workerUlid, int instanceId,
+        string claimUlid, string? lastClaimUlid = null)
     {
         logger.LogTrace("ClaimInstanceForWorker(workerId={workerId})", workerUlid);
 
@@ -76,7 +78,7 @@ public class WorkersDomainService(
         {
             throw new UserOperationException("New claim ulid cannot be the same as the old one");
         }
-        
+
         instance.ClaimUlid = claimUlid;
         instance.ClaimedWorkerUlid = workerUlid;
 
@@ -84,13 +86,13 @@ public class WorkersDomainService(
 
         return instance;
     }
-    
+
     public async Task<LobbyBotOptionEntity> GetAssignedGuildOptions(int instanceId)
     {
         logger.LogTrace("GetAssignedGuildOptions(instanceId={instanceId})", instanceId);
 
         var memberClaim = await db.LobbyBotClaims.FirstOrDefaultAsync(entity => entity.InstanceId == instanceId);
-        if(memberClaim is null)
+        if (memberClaim is null)
         {
             throw new EntityNotFoundException($"No member has claimed instance with id {instanceId}");
         }
@@ -103,7 +105,7 @@ public class WorkersDomainService(
 
         return await GetGuildOptionsByGuildId(memberClaim.GuildId);
     }
-    
+
     public async Task<LobbyBotOptionEntity> GetGuildOptionsByGuildId(long guildId)
     {
         logger.LogTrace("GetGuildOptionsByGuildId(guildId={guildId})", guildId);
@@ -116,7 +118,7 @@ public class WorkersDomainService(
 
         return options;
     }
-    
+
     public async Task<LobbyBotInstanceEntity> AssignInstanceToServer(MemberDdo member, long serverId)
     {
         logger.LogTrace("AssignInstanceToServer(member={member}, serverId={serverId})", member, serverId);
@@ -125,29 +127,30 @@ public class WorkersDomainService(
         {
             throw new UserOperationException("Member is not a patron and cant assign instances");
         }
-        
+
         var memberClaim = await db.LobbyBotClaims.FirstOrDefaultAsync(entity => entity.Login == member.Login);
-        if(memberClaim is not null && !CanClaimNewInstance(memberClaim))
+        if (memberClaim is not null && !CanClaimNewInstance(memberClaim))
         {
             throw new UserOperationException("Member has already claimed an instance in the last 7 days");
         }
+
         var newClaim = new LobbyBotClaimEntity
         {
             ClaimTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
             Login = member.Login,
             GuildId = serverId
         };
-        
+
         LobbyBotInstanceEntity instance;
-        
+
         // check if user has claimed another server, or server is already chosen
         var otherServerClaim = await db.LobbyBotClaims.FirstOrDefaultAsync(entity => entity.GuildId == serverId);
-        if(memberClaim is not null)
+        if (memberClaim is not null)
         {
             instance = await GetInstanceById(memberClaim.InstanceId);
             newClaim.InstanceId = memberClaim.InstanceId;
         }
-        else if(otherServerClaim is not null)
+        else if (otherServerClaim is not null)
         {
             instance = await GetInstanceById(otherServerClaim.InstanceId);
             newClaim.InstanceId = otherServerClaim.InstanceId;
@@ -168,34 +171,42 @@ public class WorkersDomainService(
 
         // add new claim
         db.LobbyBotClaims.Add(newClaim);
-        
+
         // if no guild options set, add
         var options = await db.LobbyBotOptions.FirstOrDefaultAsync(entity => entity.GuildId == serverId);
         if (options is null)
         {
+            // if there is an old palantir set, use the old invite - LEGACY
+            var existingLegacy = await db.Palantiris.FirstOrDefaultAsync(p => p.Palantir.Contains(serverId.ToString()));
+
             int invite;
-            do
+            if (existingLegacy == null)
             {
-                invite = new Random().Next(999999);
+                do
+                {
+                    invite = new Random().Next(999999);
+                } while (await db.LobbyBotOptions.AnyAsync(entity => entity.Invite == invite));
             }
-            while(await db.LobbyBotOptions.AnyAsync(entity => entity.Invite == invite));
-            
+            else invite = Convert.ToInt32(existingLegacy.Token);
+
             options = new LobbyBotOptionEntity
             {
                 GuildId = serverId,
                 Name = "Server",
-                Prefix = ">",
+                Prefix = ".",
                 ChannelId = null,
                 Invite = invite
             };
             db.LobbyBotOptions.Add(options);
         }
+
         await db.SaveChangesAsync();
 
         return instance;
     }
-    
-    public async Task<LobbyBotOptionEntity> UpdateGuildOptions(long guildId, string name, string prefix, long? channelId = null)
+
+    public async Task<LobbyBotOptionEntity> UpdateGuildOptions(long guildId, string name, string prefix,
+        long? channelId = null)
     {
         logger.LogTrace("UpdateGuildOptions(guildId={guildId}, name={name})", guildId, name);
 
@@ -220,7 +231,7 @@ public class WorkersDomainService(
         var lastClaim = DateTimeOffset.FromUnixTimeMilliseconds(claim.ClaimTimestamp);
         return DateTimeOffset.UtcNow - lastClaim > TimeSpan.FromDays(7);
     }
-    
+
     private static bool InstanceUnclaimedOrExpired(LobbyBotInstanceEntity instance)
     {
         if (instance.ClaimUlid is null)
