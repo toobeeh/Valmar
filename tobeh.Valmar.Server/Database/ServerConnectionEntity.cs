8c27a12296fb9ca55500f5fa8634a2728cee07ce
tobeh <tobiasch@gmx.at>
2024-05-08 19:13:06 +0200

    add missing entities

---
+++ tobeh.Valmar.Server/Database/ServerConnectionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tobeh.Valmar.Server.Database
+{
+    public partial class ServerConnectionEntity
+    {
+        [Key] [Column(TypeName = "int(11)")] public int Login { get; set; }
+
+        [Key]
+        [Column(TypeName = "bigint(20)")]
+        public long GuildId { get; set; }
+
+        public bool Ban { get; set; }
+    }
+}
---
+++ tobeh.Valmar.Server/Database/ServerWebhookEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tobeh.Valmar.Server.Database
+{
+    public partial class ServerWebhookEntity
+    {
+        [Key]
+        [Column(TypeName = "bigint(20)")]
+        public long GuildId { get; set; }
+
+        [StringLength(50)] public string Name { get; set; } = null!;
+        [Column(TypeName = "text")] public string Url { get; set; } = null!;
+    }
+}
