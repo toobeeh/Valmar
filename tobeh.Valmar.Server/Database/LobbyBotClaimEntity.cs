5d75aa922dcdf4d0d3dfad7c64bf7a4c5091d6ba
tobeh <tobiasch@gmx.at>
2024-05-11 14:51:43 +0200

    drop unique instance id key

--- tobeh.Valmar.Server/Database/LobbyBotClaimEntity.cs
+++ tobeh.Valmar.Server/Database/LobbyBotClaimEntity.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace tobeh.Valmar.Server.Database
 {
-    [Index(nameof(InstanceId), Name = "UniqueInstance", IsUnique = true)]
     public partial class LobbyBotClaimEntity
     {
-        [Key]
-        [Column(TypeName = "int(11)")]
-        public int Login { get; set; }
-        [Column(TypeName = "int(11)")]
-        public int InstanceId { get; set; }
-        [Column(TypeName = "bigint(20)")]
-        public long ClaimTimestamp { get; set; }
-        [Column(TypeName = "bigint(20)")]
-        public long GuildId { get; set; }
+        [Key] [Column(TypeName = "int(11)")] public int Login { get; set; }
+        [Column(TypeName = "int(11)")] public int InstanceId { get; set; }
+        [Column(TypeName = "bigint(20)")] public long ClaimTimestamp { get; set; }
+        [Column(TypeName = "bigint(20)")] public long GuildId { get; set; }
     }
 }
