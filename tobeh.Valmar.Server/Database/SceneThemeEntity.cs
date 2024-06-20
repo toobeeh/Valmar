6832f6b7252a87a43dec17982dadf3771805a31d
tobeh <tobiasch@gmx.at>
2024-06-20 17:12:29 +0200

    add scene theme entity

---
+++ tobeh.Valmar.Server/Database/SceneThemeEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tobeh.Valmar.Server.Database
+{
+    public partial class SceneThemeEntity
+    {
+        [Column(TypeName = "text")] public string Name { get; set; } = null!;
+        [Key] [Column(TypeName = "int(11)")] public int SceneId { get; set; }
+        [Key] [Column(TypeName = "int(11)")] public int Shift { get; set; }
+    }
+}
