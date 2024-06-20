0a96fd3ba9ea7c9c0f6ea6f21fb6c2e196a86b6a
tobeh <tobiasch@gmx.at>
2024-06-20 12:10:16 +0200

    fix concurrent value store fetches

--- tobeh.Valmar.Server/Domain/Classes/KeyValueStore.cs
+++ tobeh.Valmar.Server/Domain/Classes/KeyValueStore.cs
@@ -4,14 +4,33 @@ public class KeyValueStore<TKey, TValue>(TKey key, Func<TKey, Task<TValue>> retr
 {
     private TValue? _data;
     private bool _dirty = true;
+    private readonly SemaphoreSlim _fetchLock = new(1, 1);
 
     public async Task<TValue> Retrieve()
     {
-        if (_dirty || _data is null) _data = await retrieval.Invoke(key);
+        if (_dirty || _data is null)
+        {
+            await _fetchLock.WaitAsync();
+            if (_dirty || _data is null)
+            {
+                try
+                {
+                    _data = await retrieval.Invoke(key);
+                }
+                catch (Exception e)
+                {
+                    _fetchLock.Release();
+                    throw;
+                }
+            }
+
+            _fetchLock.Release();
+        }
+
         _dirty = false;
         return _data;
     }
-    
+
     public void Dirty() => _dirty = true;
     public void Clean(TValue data) => _data = data;
 }
