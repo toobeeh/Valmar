97f6a94b94e88d3654a024bb4c435325299ebf92
tobeh <tobiasch@gmx.at>
2024-06-27 15:23:52 +0200

    rebalance boost rate

--- tobeh.Valmar.Server/Domain/Implementation/DropsDomainService.cs
+++ tobeh.Valmar.Server/Domain/Implementation/DropsDomainService.cs
@@ -1,24 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using tobeh.Valmar.Server.Database;
 using tobeh.Valmar.Server.Util;
-using tobeh.Valmar.Server.Domain.Exceptions;
-using tobeh.Valmar.Server.Util.NChunkTree.Drops;
 
 namespace tobeh.Valmar.Server.Domain.Implementation;
 
 public class DropsDomainService(
-    ILogger<DropsDomainService> logger, 
+    ILogger<DropsDomainService> logger,
     PalantirContext db
-    ) : IDropsDomainService
+) : IDropsDomainService
 {
     public async Task ScheduleDrop(int delaySeconds, int? eventDropId)
     {
-        logger.LogTrace("ScheduleDrop(delaySeconds={delaySeconds}, eventDropId={eventDropId})", delaySeconds, eventDropId);
+        logger.LogTrace("ScheduleDrop(delaySeconds={delaySeconds}, eventDropId={eventDropId})", delaySeconds,
+            eventDropId);
 
         var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
         var validFrom =
             DropHelper.FormatDropTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(timestamp).AddSeconds(delaySeconds));
-        
+
         db.NextDrops.RemoveRange(db.NextDrops);
         db.NextDrops.Add(new NextDropEntity()
         {
@@ -32,10 +31,11 @@ public class DropsDomainService(
 
         await db.SaveChangesAsync();
     }
-    
+
     public Task<Tuple<int, int>> CalculateDropDelayBounds(int playerCount, double boostFactor)
     {
-        logger.LogTrace("CalculateDropDelayBounds(playerCount={playerCount}, boostFactor={boostFactor})", playerCount, boostFactor);
+        logger.LogTrace("CalculateDropDelayBounds(playerCount={playerCount}, boostFactor={boostFactor})", playerCount,
+            boostFactor);
 
         if (playerCount <= 0) playerCount = 1;
         int min = 600 / playerCount;
@@ -43,11 +43,13 @@ public class DropsDomainService(
         min += 20; // minimum offset
 
         // modify by boosts
-        min = Convert.ToInt32(Math.Round(min / boostFactor, 0));
-            
-        return Task.FromResult(new Tuple<int, int>(min, 4*min));
+        var boostModifier = Math.Pow(Math.E, boostFactor / 4);
+        var boostModifiedOffset = min - min / Math.Pow(Math.E, 0.25);
+        min = Convert.ToInt32(Math.Round(boostModifiedOffset + min / boostModifier, 0));
+
+        return Task.FromResult(new Tuple<int, int>(min, 4 * min));
     }
-    
+
     public async Task<double> GetCurrentDropBoost()
     {
         logger.LogTrace("GetCurrentDropBoost()");
