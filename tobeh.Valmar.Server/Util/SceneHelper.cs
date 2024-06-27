922a52a76ac7a085e78a7c17b46e06ccbc7ac89f
tobeh <tobiasch@gmx.at>
2024-06-27 15:48:27 +0200

    rebalance scene price

--- tobeh.Valmar.Server/Util/SceneHelper.cs
+++ tobeh.Valmar.Server/Util/SceneHelper.cs
@@ -2,8 +2,10 @@ namespace tobeh.Valmar.Server.Util;
 
 public static class SceneHelper
 {
+    private const int MaxScenePrice = 250000;
+
     public static int GetScenePrice(int possessedSceneAmount)
     {
-        return 20000 * (int)Math.Pow(2, possessedSceneAmount);
+        return Math.Min(MaxScenePrice, 20000 * (int)Math.Pow(2, possessedSceneAmount));
     }
 }
