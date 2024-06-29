220027a16ac5fa528c33a9fa749d30abb7245ac5
tobeh <tobiasch@gmx.at>
2024-06-29 23:25:48 +0200

    change scene start and cap price

--- tobeh.Valmar.Server/Util/SceneHelper.cs
+++ tobeh.Valmar.Server/Util/SceneHelper.cs
@@ -2,10 +2,10 @@ namespace tobeh.Valmar.Server.Util;
 
 public static class SceneHelper
 {
-    private const int MaxScenePrice = 250000;
+    private const int MaxScenePrice = 150000;
 
     public static int GetScenePrice(int possessedSceneAmount)
     {
-        return Math.Min(MaxScenePrice, 20000 * (int)Math.Pow(2, possessedSceneAmount));
+        return Math.Min(MaxScenePrice, 10000 * (int)Math.Pow(2, possessedSceneAmount));
     }
 }
