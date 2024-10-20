693bdbc4d814fb13c2cd3acc613a7fc6dbedc6c2
tobeh <tobiasch@gmx.at>
2024-10-20 11:33:58 +0200

    remove check for showinvite in connection

--- tobeh.Valmar.Server/Domain/Implementation/MembersDomainService.cs
+++ tobeh.Valmar.Server/Domain/Implementation/MembersDomainService.cs
@@ -350,11 +350,6 @@ public class MembersDomainService(
         var guild = await guildsService.GetGuildByInvite(serverToken);
         var options = await guildsService.GetGuildOptionsByGuildId(guild.GuildId);
 
-        if (!(options.ShowInvite ?? true))
-        {
-            throw new ApplicationException("Guild has not enabled connecting via invite");
-        }
-
         var existingConnection =
             await db.ServerConnections.FirstOrDefaultAsync(entity =>
                 entity.Login == login && entity.GuildId == guild.GuildId);
